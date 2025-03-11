6b1d85dd799c71b93472ae997f76fc754d913231
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-11 04:02:08 -0300

    add shared service and pass username in url

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../../../../host/src/app/shared/shared.module';
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -5,30 +6,28 @@ import { FloatLabelModule } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { Router } from '@angular/router';
-
-
-
+import { SharedService } from '../../../../../host/src/app/shared/shared.service';
 
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule, InputTextModule,FloatLabelModule,FormsModule, DialogModule ],
+  imports: [ButtonModule, InputTextModule,FloatLabelModule,FormsModule, DialogModule, SharedModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  valueInput = '';
+  userName = '';
   exibirModal = false;
 
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private sharedService: SharedService) {}
 
   entrar() {
-    if (this.valueInput.trim() === '') {
+    if (this.userName.trim() === '') {
       this.exibirModal = true;
     } else {
-      this.router.navigate(['/system']);
+      this.sharedService.setUser(this.userName);
+      this.router.navigate(['/system', { name: this.userName }]);
     }
   }
 
