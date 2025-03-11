69d4fba01a171a209afc3e06ade61acaa6bee033
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-11 03:01:26 -0300

    add primeNg and create login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { Router } from '@angular/router';
+
+
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ButtonModule, InputTextModule,FloatLabelModule,FormsModule, DialogModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  valueInput = '';
+  exibirModal = false;
+
+  constructor(private router: Router) {}
+
+
+  entrar() {
+    if (this.valueInput.trim() === '') {
+      this.exibirModal = true;
+    } else {
+      this.router.navigate(['/system']);
+    }
+  }
+
+  fecharModal() {
+    this.exibirModal = false;
+  }
 
 }
