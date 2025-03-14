3ac2d774a95ea6ed83d04e5d9e6216752deb79b7
Sinval Borges <sinborneto@yahoo.com.br>
2025-03-14 07:23:34 -0300

    update cypress test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,9 +1,10 @@
 describe('Página Inicial', () => {
   it('Deve carregar a página com sucesso', () => {
-    cy.visit('/');
-    cy.get('h1').should('contain.text', 'Olá, seja bem-vindo!');
-    cy.get('input[placeholder="Digite seu nome:"]').should('exist');
-    cy.contains('button', 'Entrar').should('be.visible');
-    cy.url().should('include', '/nova-pagina');
-    });
+    cy.visit('https://host-five-sigma.vercel.app/');
+    cy.get('p').should('contain.text', 'Olá, seja bem-vindo!');
+    cy.get('label[for="on_label"]').should('exist');
+    cy.get('input#on_label').type('João');
+    cy.contains('button', 'Entrar').should('be.visible').click();
+    cy.url().should('include', '/system;name=Jo%C3%A3o');
+  });
 });
